c8c841be8db4483caec26cd0983c2693c0f5d823
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2023-04-21 22:59:29 +0530

    Completed login Page UI

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${inter.className}`}>
+      <Head>
+        <link rel="shortcut icon" href="/amplifier.svg" />
+      </Head>
       <Component {...pageProps} />
     </main>
   );
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,47 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { Lobster } from "next/font/google";
 import { FcGoogle } from "react-icons/fc";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 
 const lobster = Lobster({ weight: "400", subsets: ["latin"] });
 
 const Login = () => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
   return (
     <div className="flex flex-col w-full h-screen">
-      <div className="flex justify-center items-center -space-x-5 -my-6">
-        <Image src="/amplifier.png" alt="Amplifier" height="120" width="120" />
-        <div className={`${lobster.className} text-5xl`}>Amplifier</div>
+      <div className="flex justify-center items-center -space-x-3">
+        <Image src="/amplifier.svg" alt="Amplifier" height="100" width="110" />
+        <div className={`${lobster.className} text-5xl -mt-5`}>Amplifier</div>
       </div>
-      <hr className="w-full my-6" />
-      <div className="w-full flex flex-col p-10 justify-center items-center">
+      <hr className="w-full -my-2" />
+      <div className="w-full flex flex-col p-10 my-6 justify-center items-center">
         <div className="w-1/3 flex flex-col">
-          <h1 className="text-center mb-5 font-medium">To continue, login to Amplifier.</h1>
-          <button className="flex justify-center items-center space-x-2 p-4 rounded-full border-green-500 border hover:bg-gray-950">
-            <FcGoogle /> <span>Continue With Google</span>
+          <h1 className="text-center mb-5 font-semibold">To continue, log in to Amplifier.</h1>
+          <button className="flex justify-center items-center space-x-2 p-4 rounded-full border-green-500 border-2 hover:bg-neutral-900 font-medium">
+            <FcGoogle size={25} /> <span>Continue With Google</span>
           </button>
           <div className="flex my-5 justify-center items-center">
             <hr className="w-full" />
-            <span className="mx-6">OR</span>
+            <span className="mx-6 font-bold">OR</span>
             <hr className="w-full" />
           </div>
           <div>
             <div className="flex flex-col">
-              <h1 className="p-2">Email address or username</h1>
-              <input type="text" placeholder="Email address or username" className="rounded-md w-full p-4"></input>
+              <h1 className="py-2">Email address</h1>
+              <input type="text" placeholder="Email address" className="rounded-md w-full p-4 text-black" />
             </div>
             <div className="flex flex-col my-4">
-              <h1 className="p-2">Password</h1>
-              <input type="text" placeholder="Password" className="rounded-md w-full p-4"></input>
+              <h1 className="py-2">Password</h1>
+              <div className="relative">
+                <input type={isPasswordVisible ? "text" : "password"} placeholder="Password" className="rounded-md w-full p-4 text-black pr-11" />
+                <div
+                  onClick={() => {
+                    setIsPasswordVisible((isPasswordVisible) => !isPasswordVisible);
+                  }}
+                >
+                  {isPasswordVisible ? (
+                    <AiFillEye className="text-black absolute top-1 right-2 translate-y-1/2 hover:cursor-pointer" size={25} />
+                  ) : (
+                    <AiFillEyeInvisible className="text-black absolute top-1 right-2 translate-y-1/2 hover:cursor-pointer" size={25} />
+                  )}
+                </div>
+              </div>
             </div>
             <div className="pt-4">
-              <button className="rounded-full hover:bg-green-500 hover:border-green-200 bg-green-600 border border-green-900 w-full p-4 font-black text-lg text-black">Login</button>
+              <button className="rounded-full bg-green-500 hover:bg-green-600 w-full p-4 text-lg text-neutral-800 font-extrabold hover:text-black">Log In</button>
             </div>
-          </div> 
-          <hr className="mt-10" />
-          <h1 className="text-center mt-6 mb-4 font-medium">Don&apos;t have an account?</h1>
-          <button className="flex justify-center items-center space-x-2 p-4 rounded-full border-green-500 border hover:bg-gray-950">
-            Sign Up For Amplifier
-          </button>
+          </div>
+          <hr className="mt-7" />
+          <h1 className="text-center mt-8 mb-4 font-semibold">Don&apos;t have an account?</h1>
+          <button className="flex justify-center items-center space-x-2 p-4 rounded-full border-green-500 border-2 hover:bg-neutral-900 font-medium">Sign Up For Amplifier</button>
         </div>
       </div>
     </div>
