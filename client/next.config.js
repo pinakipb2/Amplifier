d18b6f80ef558c9fdd9e3e6c60becba1357bda52
Sakshi Gairola <sakshi.g642001@gmail.com>
2023-04-24 12:25:58 +0530

    changes in docker compose

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
+  // webpack: (config) => {
+  //   config.watchOptions = {
+  //     poll: 1000,
+  //     aggregateTimeout: 300,
+  //   };
+  //   return config;
+  // },
+  output: "standalone",
   images: {
-    domains: ["api.dicebear.com","res.cloudinary.com"],
+    domains: ["api.dicebear.com", "res.cloudinary.com"],
   },
 };
 
